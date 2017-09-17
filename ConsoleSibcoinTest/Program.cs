c2f64bf658c5f7a01219718f14df42b4082209fa
kusokdobra <32040521+kusokdobra@users.noreply.github.com>
2017-09-18 02:49:18 +0900

    Add files via upload

---
+++ ConsoleSibcoinTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SibcoinNet;
+using System.Net;
+
+namespace ConsoleSibcoinTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SibcoinClient bc = new SibcoinClient("http://127.0.0.1:8332");
+            bc.Credentials = new NetworkCredential("alice", "password");
+            var p = bc.GetBalance();
+            Console.WriteLine(p);
+
+            Console.ReadKey();
+        }
+    }
+}
